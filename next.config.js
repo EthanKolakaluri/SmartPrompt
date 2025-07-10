f5d3dc72639ccd700f4ac8b5cd8ae7cd9133b96f
Ethan Kolakaluri <102549980+EthanKolakaluri@users.noreply.github.com>
2025-07-10 16:24:41 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const nextConfig = {
+  webpack(config) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'webassembly/async',
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
