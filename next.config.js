2b8bbb87f8d0089469243419bcdce50e73e7c311
Ethan Kolakaluri <102549980+EthanKolakaluri@users.noreply.github.com>
2025-07-10 16:31:46 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,28 @@
 const nextConfig = {
-  webpack(config) {
+  webpack(config, { isServer }) {
+    // WASM support
     config.experiments = {
       asyncWebAssembly: true,
       layers: true,
     };
-    
+
+    // WASM loader rule
     config.module.rules.push({
       test: /\.wasm$/,
       type: 'webassembly/async',
     });
 
+    // Optional: Fixes "Module not found" errors for WASM in serverless environments
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+
     return config;
   },
-};
-
-module.exports = nextConfig;
+  // For Next.js 13+
+  experimental: {
+    serverComponentsExternalPackages: ['@dqbd/tiktoken'],
+  },
